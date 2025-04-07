c5dd80a3f30ad59f27c527ea6a4a69540c13d90b
GustavoCSUG <gustavo.herrera@csuglobal.edu>
2025-04-06 20:13:43 -0700

    Critical_thinking1

---
+++ Person.java
@@ -0,0 +1,22 @@
+package critical_thinking1;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		// declare variable type and also add values to variables.
+		
+		String firstName = "John";
+		String lastName = "Doe";
+		String Address = "123 bridge ln";
+		String City = "Turlock";
+		String zipCode = "92736";
+		
+		// print a statement using variables and string concatenation.
+		
+		System.out.println("First Name: " + firstName + "\nLast Name: " + lastName + "\nAddress: " + Address + "\nCity: "+ City + "\nZipcode: " + zipCode);
+		
+
+	}
+
+}
